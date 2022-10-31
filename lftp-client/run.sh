370f14158de151e4ba46af6fb53796bf3fcf88f0
fullbright <full3right@gmail.com>
2022-10-31 20:44:27 +0100

    Create run.sh

---
+++ lftp-client/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Running lftp for server $HOST and user $USER into ftp directory $PUB_FTP_DIR"
+lftp -e "set ftp:use-site-utime2 false; mirror -x ^\.git/$ -X flat-logo.png -p -R ftp-php-ap $PUB_FTP_DIR/ftp-php-app; exit" -u $USER,$PASSWORD $HOST
