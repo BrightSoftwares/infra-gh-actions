c0b2a56e485c91355cfc590ec9c713b4f7a8cc84
fullbright <full3right@gmail.com>
2022-10-31 22:05:46 +0100

    Update run.sh

--- lftp-client/run.sh
+++ lftp-client/run.sh
@@ -2,5 +2,5 @@
 
 LOCAL_DIR_UPLOAD=.
 
-echo "Running lftp for server ${HOST} and user ${USER} into ftp directory ${PUB_FTP_DIR}"
+echo "Running lftp for server ${HOST} and user ${USER} from ${LOCAL_DIR_UPLOAD} into ftp directory ${PUB_FTP_DIR}"
 lftp -e "set ftp:use-site-utime2 false; mirror -x ^\.git/$ -X flat-logo.png -p -R ${LOCAL_DIR_UPLOAD} ${PUB_FTP_DIR}/ftp-php-app; exit" -u ${USER},${PASSWORD} ${HOST}
