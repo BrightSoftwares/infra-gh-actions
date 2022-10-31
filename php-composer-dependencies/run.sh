c8c3234841bfb3127e436e46f208346b56c83ab0
fullbright <full3right@gmail.com>
2022-10-31 21:34:11 +0100

    Update run.sh

--- php-composer-dependencies/run.sh
+++ php-composer-dependencies/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Changing directoty to $(INPUT_APPDIR)"
+cd $(INPUT_APPDIR)
+
 echo "Installing php dependencies using composer"
 composer install --ignore-platform-reqs --no-interaction --no-plugins --no-scripts --prefer-dist
